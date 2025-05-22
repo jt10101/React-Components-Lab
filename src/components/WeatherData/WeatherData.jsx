5b64af7227260b53b0494f30a0c1f60047a0e01a
Jacob Tham <jacobtham3@gmail.com>
2025-05-22 20:28:42 +0800

    components split but not properly ordered

--- src/components/WeatherData/WeatherData.jsx
+++ src/components/WeatherData/WeatherData.jsx
@@ -1,5 +1,15 @@
-const RenderData = ({ day }) => {
-  return <h2>{day}</h2>;
+const RenderData = ({ day, conditions, time }) => {
+  return (
+    <div>
+      <h2>{day}</h2>
+      <p>
+        <span>Conditions: {conditions} </span>
+      </p>
+      <p>
+        <span>Time: {time}</span>
+      </p>
+    </div>
+  );
 };
 
 export default RenderData;
--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -5,16 +5,8 @@ import "./WeatherForecast.css";
 function RenderForecast({ day, img, imgAlt, time, conditions }) {
   return (
     <div className="weather">
-      {/* <h2>{day}</h2> */}
-      <RenderData day={day} />
       <RenderImage img={img} imgAlt={imgAlt} />
-
-      <p>
-        <span>conditions: {conditions} </span>
-      </p>
-      <p>
-        <span>time: {time}</span>
-      </p>
+      <RenderData day={day} time={time} conditions={conditions} />
     </div>
   );
 }
